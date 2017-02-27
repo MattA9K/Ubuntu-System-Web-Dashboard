3b3f59fde46cf1dbfac4aac46248bd9b2f535c5a
Matt Andrzejczuk <Matt@jawn.it>
2017-02-27 07:38:19 -0800

    updated memory ticker.

--- static/app/main/apps/dashboards/server/dashboard-server.controller.js
+++ static/app/main/apps/dashboards/server/dashboard-server.controller.js
@@ -25,9 +25,9 @@
                 url: '/main/ram?format=json'
             }).then(function successCallback(response) {
                 console.log(response.data.free);
-                vm.widget1.chart.data[0]["values"].push({"x": vm.RAM, "y": response.data.free, "series": 0});
-                vm.widget1.chart.data[1]["values"].push({"x": vm.RAM, "y": response.data.available, "series": 1});
-                vm.widget1.chart.data[2]["values"].push({"x": vm.RAM, "y": response.data.total, "series": 2});
+                vm.widget1.chart.data[0]["values"].push({"x": vm.RAM, "y": (response.data.free / 1024), "series": 0});
+                vm.widget1.chart.data[1]["values"].push({"x": vm.RAM, "y": (response.data.available / 1024), "series": 1});
+                vm.widget1.chart.data[2]["values"].push({"x": vm.RAM, "y": ((response.data.total / 1024) - (response.data.available / 1024)), "series": 2});
                 vm.RAM += 1;
             }, function errorCallback(response) {
 
@@ -67,7 +67,7 @@
                         },
                         yAxis: {
                             tickFormat: function (d) {
-                                return d + ' KB';
+                                return d + ' MB';
                             }
                         },
                         interactiveLayer: {
