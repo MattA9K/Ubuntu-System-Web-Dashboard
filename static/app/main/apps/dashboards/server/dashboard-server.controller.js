05d850c7c2b7a5d3df9d7253919ce20eb6ccc8e5
Matt Andrzejczuk <Matt@jawn.it>
2017-02-27 16:53:02 -0800

    some more stuffs

--- static/app/main/apps/dashboards/server/dashboard-server.controller.js
+++ static/app/main/apps/dashboards/server/dashboard-server.controller.js
@@ -26,29 +26,26 @@
                 console.log(response.data.free);
 
                 var freeMB = response.data.free / 1024;
-                var freeGB = freeMB / 1024;
 
                 vm.widget1.chart.data[0]["values"].push({
                     "x": vm.RAM,
-                    "y": (freeGB),
+                    "y": (freeMB),
                     "series": 0
                 });
 
                 var availableMB = response.data.available / 1024;
-                var availableGB = availableMB / 1024;
 
                 vm.widget1.chart.data[1]["values"].push({
                     "x": vm.RAM,
-                    "y": (availableGB),
+                    "y": (availableMB),
                     "series": 1
                 });
 
                 var totalMB = response.data.total / 1024;
-                var totalGB = totalMB / 1024;
 
                 vm.widget1.chart.data[2]["values"].push({
                     "x": vm.RAM,
-                    "y": (totalGB - availableGB),
+                    "y": (totalMB - availableMB),
                     "series": 2
                 });
                 vm.RAM += 1;
